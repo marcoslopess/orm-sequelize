d2b8168e979a881000f6978ba0988e4eea6d6aa5
Marcos Lopes <marcospaulolopesc@gmail.com>
2023-11-06 15:09:50 -0300

    aula2

--- arquivos_base/seed-matriculas.js
+++ arquivos_base/seed-matriculas.js
@@ -3,43 +3,43 @@ module.exports = {
 		return queryInterface.bulkInsert('Matriculas', [
 			{
 				status: "confirmado",
-				estudante_id: 1,
-				turma_id: 1,
+				id_aluno: 1,
+				id_turma: 1,
 				createdAt: new Date(),
 				updatedAt: new Date()
 		},
 		{
 			status: "confirmado",
-			estudante_id: 2,
-			turma_id: 1,
+			id_aluno: 2,
+			id_turma: 1,
 			createdAt: new Date(),
 			updatedAt: new Date()
 	},
 		{
 			status: "confirmado",
-			estudante_id: 3,
-			turma_id: 2,
+			id_aluno: 3,
+			id_turma: 2,
 			createdAt: new Date(),
 			updatedAt: new Date()
 	},
 		{
 			status: "confirmado",
-			estudante_id: 4,
-			turma_id: 3,
+			id_aluno: 4,
+			id_turma: 3,
 			createdAt: new Date(),
 			updatedAt: new Date()
 	},
 		{
 			status: "cancelado",
-			estudante_id: 1,
-			turma_id: 2,
+			id_aluno: 1,
+			id_turma: 2,
 			createdAt: new Date(),
 			updatedAt: new Date()
 	},
 		{
 			status: "cancelado",
-			estudante_id: 2,
-			turma_id: 2,
+			id_aluno: 2,
+			id_turma: 2,
 			createdAt: new Date(),
 			updatedAt: new Date()
 		}
