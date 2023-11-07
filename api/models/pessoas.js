93edbb60d219a78a03198fd74b36ee8f0d2cd279
Marcos Lopes <marcospaulolopesc@gmail.com>
2023-11-07 18:30:53 -0300

    aula 3

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -41,6 +41,10 @@ module.exports = (sequelize, DataTypes) => {
     });
     Pessoas.hasMany(models.Matriculas, {
       foreignKey: "estudante_id",
+      scope: {
+        status: "confirmado",
+      },
+      as: "aulasMatriculadas",
     });
   };
   return Pessoas;
