c46464f148fcbff13dba3f4e46ef666d1fb095bb
Marcos Lopes <marcospaulolopesc@gmail.com>
2023-11-07 09:06:15 -0300

    FIM DO CURSO BASICO DO ORM SEQUELIZE E AULA UM DO CURSO AVANÇADO

--- api/models/matriculas.js
+++ api/models/matriculas.js
@@ -1,12 +1,19 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Matriculas = sequelize.define('Matriculas', {
-    status: DataTypes.STRING
-  }, {});
-  Matriculas.associate = function(models) {
-    Matriculas.belongsTo(models.Pessoas)
-    Matriculas.belongsTo(models.Turmas)
-
+  const Matriculas = sequelize.define(
+    "Matriculas",
+    {
+      status: DataTypes.STRING,
+    },
+    { paranoid: true }
+  );
+  Matriculas.associate = function (models) {
+    Matriculas.belongsTo(models.Pessoas, {
+      foreignKey: "estudante_id",
+    });
+    Matriculas.belongsTo(models.Turmas, {
+      foreignKey: "turma_id",
+    });
   };
   return Matriculas;
 };
--- api/models/niveis.js
+++ api/models/niveis.js
@@ -1,12 +1,16 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Niveis = sequelize.define('Niveis', {
-    descr_nivel: DataTypes.STRING
-  }, {});
-  Niveis.associate = function(models) {
+  const Niveis = sequelize.define(
+    "Niveis",
+    {
+      descr_nivel: DataTypes.STRING,
+    },
+    { paranoid: true }
+  );
+  Niveis.associate = function (models) {
     Niveis.hasMany(models.Turmas, {
-      foreignKey: 'nivel_id'
-    })
+      foreignKey: "nivel_id",
+    });
   };
   return Niveis;
 };
